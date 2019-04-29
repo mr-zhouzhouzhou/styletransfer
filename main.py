5f8707ea039e9a03871d9df035a88177e5e637e4
Your Name <1801210925@pku.edu.cn>
2019-04-29 23:44:11 +0800

    display

--- main.py
+++ main.py
@@ -3,15 +3,8 @@ from core import app as model
 import random
 import base64
 import argparse
-
-#########################################################
-#将根目录加入sys.path中,解决命令行找不到包的问题
-import sys
 import os
-curPath = os.path.abspath(os.path.dirname(__file__))
-rootPath = os.path.split(curPath)[0]
-sys.path.append(rootPath)
-#########################################################
+
 
 parser = argparse.ArgumentParser()  # 定义一个参数设置器
 # 固定参数
